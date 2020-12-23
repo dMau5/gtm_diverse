c74644b9fe67717dedebeba7b81a887512fd3a51
Динар <45795277+dMau5@users.noreply.github.com>
2020-12-23 14:09:05 +0300

    Add files via upload

---
+++ gtm_diverse.py
@@ -0,0 +1,22 @@
+import base64
+from io import BytesIO
+from rdkit import SimDivFilters
+from rdkit.Chem import ForwardSDMolSupplier, rdMolDescriptors
+
+
+def gtm_diverse(files, count):
+    molecules = []
+    molecules_fps = []
+    for content in files:
+        data = content.encode("utf8").split(b";base64,")[1]
+        suppl = ForwardSDMolSupplier(BytesIO(base64.decodebytes(data)))
+        for mol in suppl:
+            try:
+                molecules_fps.append(rdMolDescriptors.GetMorganFingerprintAsBitVect(mol, 2))
+                molecules.append(mol)
+            except:
+                pass
+
+    mmp = SimDivFilters.MaxMinPicker()
+    picks = mmp.LazyBitVectorPick(molecules_fps, len(molecules_fps), count)
+    return [molecules[i] for i in picks]
